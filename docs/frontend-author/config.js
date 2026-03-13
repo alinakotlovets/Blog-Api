4a2c9a19a23c6a3dcd1d1ce5d6c86565c36fecad
Alina <161679516+alinakotlovets@users.noreply.github.com>
2026-03-13 19:47:42 +0200

    Update config.js

--- docs/frontend-author/config.js
+++ docs/frontend-author/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://localhost:3000";
+export const API_BASE = "https://blog-api-woad-gamma.vercel.app/";
 export const READER_FRONT = "https://alinakotlovets.github.io/Blog-Api/frontend-reader"
