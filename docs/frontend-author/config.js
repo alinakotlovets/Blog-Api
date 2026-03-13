5a4ce073146758f0a5cdb3d7ab60714b722cc6b0
Alina <161679516+alinakotlovets@users.noreply.github.com>
2026-03-13 19:44:16 +0200

    Update config.js
    change frontend reader link

--- docs/frontend-author/config.js
+++ docs/frontend-author/config.js
@@ -1,2 +1,2 @@
 export const API_BASE = "http://localhost:3000";
-export const READER_FRONT = "http://localhost:63342/Blog/frontend-reader"
+export const READER_FRONT = "https://alinakotlovets.github.io/Blog-Api/frontend-reader"
