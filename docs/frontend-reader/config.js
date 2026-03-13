2bc2ca5daa0792f72f90e770dc71093b33812c30
Alina <161679516+alinakotlovets@users.noreply.github.com>
2026-03-13 19:45:16 +0200

    Update config.js
    change author fronted link

--- docs/frontend-reader/config.js
+++ docs/frontend-reader/config.js
@@ -1,2 +1,2 @@
 export const API_BASE = "http://localhost:3000";
-export const AUTHOR_LINK = "http://localhost:63342/Blog/frontend-author"
+export const AUTHOR_LINK = "https://alinakotlovets.github.io/Blog-Api/frontend-author"
