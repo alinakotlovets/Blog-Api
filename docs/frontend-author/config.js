4e17f3dc1463abb052b20f8b5f6f703cef1bc143
Alina <161679516+alinakotlovets@users.noreply.github.com>
2026-03-13 20:04:34 +0200

    Update config.js
    change API_BASE

--- docs/frontend-author/config.js
+++ docs/frontend-author/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "https://blog-api-woad-gamma.vercel.app/";
+export const API_BASE = "https://blog-api-8rcb.onrender.com";
 export const READER_FRONT = "https://alinakotlovets.github.io/Blog-Api/frontend-reader"
