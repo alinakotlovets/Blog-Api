0287b7f7641693d33f99b650e8be24bcb697ac02
Alina <161679516+alinakotlovets@users.noreply.github.com>
2026-03-13 20:04:05 +0200

    Update config.js
    change backend link

--- docs/frontend-reader/config.js
+++ docs/frontend-reader/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "https://blog-api-woad-gamma.vercel.app/";
+export const API_BASE = "https://blog-api-8rcb.onrender.com";
 export const AUTHOR_LINK = "https://alinakotlovets.github.io/Blog-Api/frontend-author"
